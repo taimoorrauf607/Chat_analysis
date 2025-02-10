61ecfa5f8a68e54c8e1ab6e492ccce61d978ef44
temii <taimoorrauf607@gmail.com>
2025-02-10 21:22:29 +0500

    comment wordcloud becuase it make processing slow in live web app

--- app.py
+++ app.py
@@ -82,16 +82,16 @@ if uploaded_file is not None:
         st.pyplot(fig)
 
         # # Make a wordcloud of most common words
-        words = helper.make_wordcloud(select_user,df)
-        def generate_wordcloud(input):
-            cloud = WordCloud(width=500, height=500, max_words=500, background_color='black', colormap='coolwarm')
-            wordcloud = cloud.generate(input.str.cat(sep=" "))
-            plt.figure(figsize=(10, 8))
-            plt.imshow(wordcloud, interpolation='bilinear')
-            plt.axis('off')
-            plt.tight_layout()
-            st.pyplot(plt)
-        generate_wordcloud(words)
+        # words = helper.make_wordcloud(select_user,df)
+        # def generate_wordcloud(input):
+        #     cloud = WordCloud(width=500, height=500, max_words=500, background_color='black', colormap='coolwarm')
+        #     wordcloud = cloud.generate(input.str.cat(sep=" "))
+        #     plt.figure(figsize=(10, 8))
+        #     plt.imshow(wordcloud, interpolation='bilinear')
+        #     plt.axis('off')
+        #     plt.tight_layout()
+        #     st.pyplot(plt)
+        # generate_wordcloud(words)
         
         # wc = WordCloud(width=500,height=500,max_words=400,background_color='white',colormap='viridis')
         # wc_df = wc.generate(words.str.cat(sep=" "))
